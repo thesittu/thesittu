b6185aeabc781f9e3b8ad223765b03c2c381f053
root <root@vps.server.local>
2020-05-26 17:39:54 +0000

    Updates

---
+++ precache-manifest.3c284bc9e8d5d8d9943dc81082f2359e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c88f801c80aa13afff454fc87fc2be8",
+    "url": "/thesittu/index.html"
+  },
+  {
+    "revision": "4294d598d79df5bbeb73",
+    "url": "/thesittu/static/css/main.8f70c4b6.chunk.css"
+  },
+  {
+    "revision": "d04ee35f74cd0dc7481e",
+    "url": "/thesittu/static/js/2.73996ebb.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/thesittu/static/js/2.73996ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4294d598d79df5bbeb73",
+    "url": "/thesittu/static/js/main.6c64826f.chunk.js"
+  },
+  {
+    "revision": "986cfa87ce7fb673484a",
+    "url": "/thesittu/static/js/runtime-main.073f2b8c.js"
+  }
+]);
